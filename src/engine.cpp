953f361b27d2f0b6df99390b048c5af204790c88
Marco G. Maia <marcogmaia@gmail.com>
2021-12-30 15:55:45 -0300

    remove identical branches

--- src/engine.cpp
+++ src/engine.cpp
@@ -16,56 +16,34 @@ void Engine::PollEvents() {
       case sf::Event::Closed: {
         window_.close();
       } break;
-      case sf::Event::Resized:
-        break;
-      case sf::Event::LostFocus:
-        break;
-      case sf::Event::GainedFocus:
-        break;
-      case sf::Event::TextEntered:
-        break;
       case sf::Event::KeyPressed: {
         get_input_handler().PushKey(event.key.code);
       } break;
+      case sf::Event::Resized:
+      case sf::Event::LostFocus:
+      case sf::Event::GainedFocus:
+      case sf::Event::TextEntered:
       case sf::Event::KeyReleased:
-        break;
       case sf::Event::MouseWheelScrolled:
-        break;
       case sf::Event::MouseButtonPressed:
-        break;
       case sf::Event::MouseButtonReleased:
-        break;
       case sf::Event::MouseMoved:
-        break;
       case sf::Event::MouseEntered:
-        break;
       case sf::Event::MouseLeft:
-        break;
       case sf::Event::JoystickButtonPressed:
-        break;
       case sf::Event::JoystickButtonReleased:
-        break;
       case sf::Event::JoystickMoved:
-        break;
       case sf::Event::JoystickConnected:
-        break;
       case sf::Event::JoystickDisconnected:
-        break;
       case sf::Event::TouchBegan:
-        break;
       case sf::Event::TouchMoved:
-        break;
       case sf::Event::TouchEnded:
-        break;
       case sf::Event::SensorChanged:
-        break;
       case sf::Event::Count:
-        break;
       default:
         break;
     }
   }
 }
-
 
 }  // namespace sn
